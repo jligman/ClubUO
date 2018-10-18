68a97a67c019eaac9b80b05821aefb8f5054aef0
jligman <josh@spudhub.biz>
2018-10-18 19:20:37 +0000

    Created index.html

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+const ip = process.env.IP; //comment out for production
+const port = process.env.PORT; //Change to 3000 for production, process.env.port for testing
+
+//=========****REMOVE IP FOR PRODUCTION****============
+app.listen(port, ip, function() {
+  console.log('listening on ' + port);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
